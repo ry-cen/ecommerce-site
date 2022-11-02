e2b025669b1fb57412d6d3d48f5514bb03ca130e
Ryan Cen <rycen618@gmail.com>
2022-11-01 18:44:27 -0700

    database

---
+++ pages/api/seed.js
@@ -0,0 +1,15 @@
+import db from '../../utils/db';
+import User from '../../models/user';
+import data from '../../utils/data';
+
+const handler = async (req, res) => {
+  await db.connect();
+  await User.deleteMany();
+  await User.insertMany(data.users);
+  await db.disconnect;
+
+  res.send({ message: 'seeded successfully' });
+
+}
+
+export default handler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,17 @@ import type { NextPage } from 'next'
 import ProductCard from '../components/productCard'
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
+import data from '../utils/data.js'
 
 const Home: NextPage = () => {
 
-  const products = [{ name: 'pants', price: 5, imagesrc: '/../public/unknown (4).png', pid: '123' },
-  { name: 'dogshljglkhlkhlkjhlkj', price: 5, imagesrc: '/../public/unknown (4).png', pid: '23' },
-  { name: 'dra', price: 5, imagesrc: '/../public/unknown (4).png', pid: '13253223' },
-  { name: 'dfg', price: 5, imagesrc: '/../public/unknown (4).png', pid: '153423423' },
-  { name: 'dogs', price: 5, imagesrc: '/../public/unknown (4).png', pid: '23' },
-  { name: 'dra', price: 5, imagesrc: '/../public/unknown (4).png', pid: '13253223' },
-  { name: 'dfg', price: 5, imagesrc: '/../public/unknown (4).png', pid: '153423423' },
-  { name: 'dogs', price: 5, imagesrc: '/../public/unknown (4).png', pid: '23' },
-  { name: 'dra', price: 5, imagesrc: '/../public/unknown (4).png', pid: '13253223' },
-  { name: 'dfg', price: 5, imagesrc: '/../public/unknown (4).png', pid: '153423423' }]
+
 
   return (
     <Layout>
     <div className={styles.container}>
       <div className={styles.flex_box}>
-          {products.map((product, index) => 
+          {data.products.map((product, index) => 
              <ProductCard key={index} name={product.name} price={product.price} imagesrc={product.imagesrc} pid={product.pid}></ProductCard>
           )}
       </div>
